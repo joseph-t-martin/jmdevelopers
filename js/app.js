f8d79b27549cad82dabf8eab8b96149612eab991
Joseph <wma_10@hotmail.com>
2017-04-30 17:09:48 +1000

    Adding feature to submit form

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ReactRouter from 'react-router';
 
 import Header from './components/header.js';
 import Home from './components/home.js';
--- js/components/contact_us.js
+++ js/components/contact_us.js
@@ -1,6 +1,50 @@
 import React from 'react';
 
 class ContactUs extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name : '',
+            email : '',
+            message : '',
+        };
+
+        this.type = this.type.bind(this);
+        this.addLead = this.addLead.bind(this);
+
+    };
+
+    type(e){
+        this.setState({[e.target.name]: e.target.value});
+    };
+
+    addLead() {
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: "http://backend.local/api/leads/store",
+            data: {
+                name: this.state.name,
+                email: this.state.email,
+                message: this.state.message
+            },
+            success: function() {
+                this.popup('Message sent');
+                this.setState({name: '', email: '', message: ''});
+            }.bind(this),
+            error: function() {
+                this.popup('Error sending message. Please try again');
+            }.bind(this),
+        });
+    };
+
+    popup(message) {
+        let response = $('<span>' + message + '</span>');
+        Materialize.toast(response, 5000);
+    }
+
     render() {
         return (
             <div>
@@ -11,26 +55,26 @@ class ContactUs extends React.Component {
                     <div className="row">
                         <div className="input-field col s6">
                             <i className="material-icons prefix">account_circle</i>
-                            <input id="icon_prefix" type="text" className="validate" />
-                            <label htmlFor="icon_prefix">Name</label>
+                            <label htmlFor="name">Name</label>
+                            <input name="name" type="text" className="validate" value={this.state.name} onChange={this.type} />
                         </div>
                     </div>
                     <div className="row">
                         <div className="input-field col s6">
                             <i className="material-icons prefix">email</i>
-                            <input id="email" type="email" className="validate" />
                             <label htmlFor="email">Email</label>
+                            <input name="email" type="email" className="validate" value={this.state.email} onChange={this.type} />
                         </div>
                     </div>
                     <div className="row">
                         <div className="input-field col s6">
                             <i className="material-icons prefix">mode_edit</i>
-                            <textarea id="icon_prefix2" className="materialize-textarea"></textarea>
-                            <label htmlFor="icon_prefix2">Message</label>
+                            <label htmlFor="message">Message</label>
+                            <textarea name="message" className="materialize-textarea" value={this.state.message} onChange={this.type} />
                         </div>
                     </div>
                     <div className="row">
-                        <button className="btn waves-effect waves-light" name="action">Send Message
+                        <button onClick={this.addLead} className="btn waves-effect waves-light" name="action">Send Message
                             <i className="material-icons right">send</i>
                         </button>
                     </div>
